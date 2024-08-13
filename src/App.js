5c628f18b958c76001414c1de1199704ca1585a4
Karan Dixit <dixitk941@gmail.com>
2024-08-14 02:09:37 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Features from './pages/Features';
 import GithubIntegration from './pages/GithubIntegration';
 import ContactForm from './pages/ContactForm';
 import About from './pages/About';
+import Room from './pages/Room'
 import Header from './components/Header';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
             <Route path="/features" element={<Features />} />
             <Route path="/contact" element={<ContactForm />} />
             <Route path="/about" element={<About />} />
+            <Route path="/room/:roomId" element={<Room />} />
             <Route path="/githubintegration" element={<GithubIntegration />} />
           </Routes>
         </main>
